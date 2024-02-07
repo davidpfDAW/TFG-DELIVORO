1cc1062c557ffa0be312f2779dd187ccab5cf5b5
davidpfDAW <113421998+davidpfDAW@users.noreply.github.com>
2024-02-07 00:19:18 +0000

    Arreglo password expuesta

--- api/config.php
+++ api/config.php
@@ -1,6 +1,6 @@
 <?php
     define("DB_HOST", "localhost");
     define("DB_USERNAME", "root");
-    define("DB_PASSWORD", "Yenkyone2002");
+    define("DB_PASSWORD", "passw");
     define("DB_DATABASE_NAME", "delivoro");
 ?>
